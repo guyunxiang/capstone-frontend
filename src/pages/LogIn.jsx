e6bc71bb6b64388e80ae83ce1aa886ff6fe166bf
guyunxiang <guyunxiang32@gmail.com>
2024-11-21 14:23:55 -0500

    fixed api routes

--- src/pages/LogIn.jsx
+++ src/pages/LogIn.jsx
@@ -16,7 +16,7 @@ const Login = () => {
   const handleSubmit = async (e) => {
     e.preventDefault();
     try {
-      const response = await fetch("/api/login", {
+      const response = await fetch("/api/users/login", {
         method: "POST",
         headers: {
           "Content-Type": "application/json",
