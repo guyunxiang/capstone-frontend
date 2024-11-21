021c12bbfb60e22b266e5cc1fc39a9a36e72d0fd
gurleen1277 <gk184151@gmail.com>
2024-11-21 17:35:30 -0500

    update home page hero image responsive

--- src/components/Home/Hero.jsx
+++ src/components/Home/Hero.jsx
@@ -1,8 +1,9 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 const Hero = () => {
     return (
-        <div className="h-[75vh] flex flex-col lg:flex-row px-4 lg:px-8">
+        <div className="h-auto lg:h-[75vh] flex flex-col lg:flex-row px-4 lg:px-8">
             {/* Text Section */}
             <div className="w-full lg:w-3/6 flex flex-col items-center lg:items-start justify-center text-center lg:text-left">
                 <h1 className="text-3xl md:text-4xl lg:text-6xl font-semibold text-yellow-100">
@@ -12,15 +13,21 @@ const Hero = () => {
                     From thrilling mysteries to insightful nonfiction, find your perfect book here.
                 </p>
                 <div className="mt-8">
-                    <button className="px-6 py-3 text-yellow-100 text-lg md:text-xl lg:text-2xl font-semibold border border-yellow-100 rounded-full transition-all duration-300 hover:bg-yellow-100 hover:text-zinc-800">
-                        Discover Books
-                    </button>
+                    <Link to="/all-books">
+                        <button className="px-6 py-3 text-yellow-100 text-lg md:text-xl lg:text-2xl font-semibold border border-yellow-100 rounded-full transition-all duration-300 hover:bg-yellow-100 hover:text-zinc-800">
+                            Discover Books
+                        </button>
+                    </Link> 
                 </div>
             </div>
             
             {/* Image Section */}
             <div className="w-full lg:w-3/6 flex items-center justify-center mt-8 lg:mt-0">
-                <img src="./hero.jpg" alt="hero" className="h-48 md:h-72 lg:h-full object-contain" />
+                <img 
+                    src="./hero.jpg" 
+                    alt="hero" 
+                    className="w-full h-auto lg:h-full object-contain" 
+                />
             </div>
         </div>
     );
