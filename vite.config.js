c3ef961c8b0335a43f8a45a470ec8107c299b801
guyunxiang <guyunxiang32@gmail.com>
2024-12-03 00:42:35 -0500

    update index template

--- vite.config.js
+++ vite.config.js
@@ -1,22 +1,22 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react-swc";
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
   resolve: {
     alias: {
-      '@': '/src',
+      "@": "/src",
     },
   },
   server: {
     proxy: {
       // Proxy API calls to your backend server
-      '/api': {
-        target: 'http://localhost:4000', // Replace with your backend server URL
+      "/api": {
+        target: "https://capstone-backend-2qci.onrender.com", // Replace with your backend server URL
         changeOrigin: true,
         // rewrite: (path) => path.replace(/^\/api/, ''),
       },
     },
-  }
-})
+  },
+});
